c3e2e3fd7b1ac45c50a6e55305d8f9a19fdb0d7a
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-02 10:16:26 +0500

    corrected loader

--- src/app/(main)/loading.tsx
+++ src/app/(main)/loading.tsx
@@ -2,26 +2,24 @@
 import { useTheme } from 'next-themes'
 import { TailSpin } from 'react-loader-spinner'
 
-const loading = () => {
+const Loading = () => {
   const { theme } = useTheme()
   return (
-    <section className="flex justify-between min-h-[81vh]">
-      <section className="flex flex-col w-[80%] px-8 relative left-[20%] z-0">
-        <div className="w-full my-auto mx-auto pl-20">
-          <TailSpin
-            height="80"
-            width="80"
-            color={`${theme === 'dark' ? '#FFFFFF' : '#000000'}`}
-            ariaLabel="tail-spin-loading"
-            radius="1"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-          />
 
-        </div>
-      </section>
-    </section>
+    <div className="w-full h-full  flex justify-center items-center">
+      <TailSpin
+        height="80"
+        width="80"
+        color={`${theme === 'dark' ? '#FFFFFF' : '#000000'}`}
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        
+      />
+    </div>
+
   )
 }
-export default loading
+export default Loading
